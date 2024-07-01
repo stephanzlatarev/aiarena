8816325c663afedfc3bd1b33f77b9ef1b6b0b284
Stephan Zlatarev <stephan.zlatarev@sap.com>
2024-07-01 19:20:15 +0300

    Add match and rankings endpoints to API

--- api/src/mongo.js
+++ api/src/mongo.js
@@ -15,9 +15,13 @@ async function connect(collection) {
 export async function getBotInfo(bot) {
   if (!bot || !bot.length) return;
 
-  const info = { bot: bot, matches: [] };
+  const info = { bot: bot, ranking: {}, matches: [] };
+
+  const rankings = await connect("rankings");
+  info.ranking = await rankings.findOne({ bot: bot });
+
   const matches = await connect("matches");
-  const matchProjection = { _id: 0, round: 1, match: 1, time: 1, map: 1, player1: 1, player2: 1, winner: 1, replay: 1, warnings: 1 };
+  const matchProjection = { _id: 0, round: 1, match: 1, time: 1, map: 1, player1: 1, player2: 1, winner: 1, replay: 1, warnings: 1, overview: 1 };
 
   const cursor = matches.find({ player1: bot }).project(matchProjection);
   while (await cursor.hasNext()) {
@@ -25,4 +29,38 @@ export async function getBotInfo(bot) {
   }
 
   return info;
+}
+
+export async function getMatchInfo(match) {
+  if (!match || !match.length) return;
+
+  const matches = await connect("matches");
+  const matchProjection = { _id: 0, round: 1, match: 1, time: 1, map: 1, player1: 1, player2: 1, winner: 1, replay: 1, warnings: 1, overview: 1, timeline: 1 };
+  const matchInfo = await matches.findOne({ match: Number(match) }, { projection: matchProjection });
+
+  const rankings = await connect("rankings");
+  
+  return {
+    ...matchInfo,
+    player1: {
+      bot: matchInfo.player1,
+      ranking: await rankings.findOne({ bot: matchInfo.player1 }),
+    },
+    player2: {
+      bot: matchInfo.player2,
+      ranking: await rankings.findOne({ bot: matchInfo.player2 }),
+    },
+  };
+}
+
+export async function getRankings() {
+  const list = [];
+  const rankings = await connect("rankings");
+
+  const cursor = rankings.find({});
+  while (await cursor.hasNext()) {
+    list.push(await cursor.next());
+  }
+
+  return list;
 }
--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,7 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import { getBotInfo } from "./mongo.js";
+import { getBotInfo, getMatchInfo, getRankings } from "./mongo.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/bot/*", async function(request, response) {
   response.json(await getBotInfo(request.params[0]));
+});
+
+app.get("/api/match/*", async function(request, response) {
+  response.json(await getMatchInfo(request.params[0]));
+});
+
+app.get("/api/rankings", async function(_, response) {
+  response.json(await getRankings());
 });
 
 export const server = app.listen(port, () => {
