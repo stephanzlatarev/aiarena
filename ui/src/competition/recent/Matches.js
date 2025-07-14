57cff33ed29e19b0da7f8b2e20b0741c30c6dbf8
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-07-14 08:51:15 +0300

    Rearrange match card in recent matches

--- ui/src/competition/recent/Matches.js
+++ ui/src/competition/recent/Matches.js
@@ -16,7 +16,8 @@ function MatchCard({ match }) {
     <Link className="match-card-link" to={ `/bot/${match.winner}/match/${match.match}` }>
       <div className="match-card">
         <span className="match-card-match">Match { match.match }</span>
-        <strong>{ match.winner }</strong> vs <strong>{ match.opponent }</strong>
+        <span className="match-card-time">{ new Date(match.time).toLocaleString() }</span>
+        <span className="match-card-players"><strong>{ match.winner }</strong> vs <strong>{ match.opponent }</strong></span>
 
         <div className="match-card-summary">{ match.summary.summary }</div>
       </div>
