65024d746920a07b254974df76ddb083cedc97a7
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-07-14 08:37:58 +0300

    Improve look-and-feel of list of recent matches

--- ui/src/competition/recent/Matches.js
+++ ui/src/competition/recent/Matches.js
@@ -1,30 +1,25 @@
 import { Link } from "react-router-dom";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
+import "./style.css";
 
 export default function Matches({ matches }) {
-  const rows = [];
-
-  for (const one of matches) {
-    rows.push(
-      <TableRow key={ one.time }>
-        <TableCell style={{ padding: "1rem" }}><Link to={ `/bot/${one.winner}/match/${one.match}` }>{ one.match }</Link></TableCell>
-        <TableCell>{ one.winner } vs { one.opponent }</TableCell>
-        <TableCell>{ one.summary.summary }</TableCell>
-      </TableRow>
-    );
-  }
+  const rows = matches.map((match) => (<MatchCard key={ match.time } match={ match } />));
 
   return (
-    <TableContainer>
-      <Table>
-        <TableBody>
-          { rows }
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      { rows }
+    </div>
+  );
+}
+
+function MatchCard({ match }) {
+  return (
+    <Link className="match-card-link" to={ `/bot/${match.winner}/match/${match.match}` }>
+      <div className="match-card">
+        <span className="match-card-match">Match { match.match }</span>
+        <strong>{ match.winner }</strong> vs <strong>{ match.opponent }</strong>
+
+        <div className="match-card-summary">{ match.summary.summary }</div>
+      </div>
+    </Link>
   );
 }
